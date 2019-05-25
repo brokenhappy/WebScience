1b03aa2480f555d1f6181dc3a9898dcdf0326eab
binduvr <b.j.vanraak@gmail.com>
2019-05-25 19:52:11 +0200

    Update GraphBuilder.java

--- GraphAnalysis/src/model/GraphBuilder.java
+++ GraphAnalysis/src/model/GraphBuilder.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,7 +18,6 @@ public class GraphBuilder {
 
 	private Map<String, Node> nodes = new HashMap<String, Node>();
 	private Map<Integer, ArrayList<String>> categories = new HashMap<Integer, ArrayList<String>>();
-	private Set<Review> reviews = new HashSet<Review>();
 
 	private Graph graph;
 
@@ -39,7 +40,7 @@ public class GraphBuilder {
 		// Create user node objects
 		MySQL.processQuery("SELECT ID, name FROM user LIMIT 1000", (row) -> {
 			int id = Integer.parseInt((String) row[0]);
-			String url = String.format("ur%07d", id);
+			String url = (String) row[0];
 			String name = (String) row[1];
 			UserContext context = new UserContext(id, name);
 			Node node = new Node(url, context);
@@ -51,7 +52,8 @@ public class GraphBuilder {
 		// Create game node objects
 		MySQL.processQuery("SELECT ID, Rating, NrOfVotes, Title FROM game", (row) -> {
 			int id = Integer.parseInt((String) row[0]);
-			String url = String.format("tt%07d", id);
+//			String url = String.format("tt%07d", id);
+			String url = (String) row[0];
 			int rating = Integer.parseInt((String) row[1]);
 			int nrOfVotes = Integer.parseInt((String) row[2]);
 			String title = (String) row[3];
@@ -62,26 +64,35 @@ public class GraphBuilder {
 		System.out.println("Done games.");
 		System.out.println(nodes.size());
 
+	}
+
+	public void genEdges() {
 		// IDK wtf im doing - Bindu
 
+		List<Review> reviews = new ArrayList<Review>();
+		
 		// Create review objects
-//		MySQL.processQuery(
-//				"SELECT user, game, rating, date, helpfullnessPositive, helpfullnessNegative FROM review LIMIT 10",
-//				(row) -> {
-//					int user = (int) row[0];
-//					int game = (int) row[1];
-//					int rating = (int) row[2];
-//					LocalDate date = LocalDate.parse((String) row[3]);
-//					int helpfulnessPositive = (int) row[4];
-//					int helpfulnessNegative = (int) row[5];
-//					Review review = new Review(user, game, rating, date, helpfulnessPositive, helpfulnessNegative);
-//
-//				});
+		MySQL.processQuery("SELECT user, game, rating, date, helpfullnessPositive, helpfullnessNegative FROM review", (row) -> {
+			int user = Integer.parseInt((String) row[0]);
+			int game = Integer.parseInt((String) row[1]);
+			int rating = Integer.parseInt((String) row[2]);
+			LocalDate date = LocalDate.parse((String) row[3]);
+			int helpfulnessPositive = Integer.parseInt((String) row[4]);
+			int helpfulnessNegative = Integer.parseInt((String) row[5]);
+			Review review = new Review(user, game, rating, date, helpfulnessPositive, helpfulnessNegative);
+			reviews.add(review);
+//			System.out.println(review.getUser());
+
+		});
+//		for(Review review : reviews) {
+//			
+//		}
 	}
 
 	public static void main(String[] args) {
 		GraphBuilder builder = new GraphBuilder();
 		builder.genGraph();
+//		System.out.println(builder.nodes.size());
 //		for(Node node : builder.nodes.values()) {
 //			GameContext con = (GameContext) node.getContext();
 //			System.out.println(node.getUrl() + " " + con.getNrOfVotes());
